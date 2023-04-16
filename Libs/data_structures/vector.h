cf5ee20270c8d1ea7cf29f33f37a6e16049d8613
PADJ3301 <danielle.chmyhov@gmail.com>
2023-04-16 07:32:07 +0300

    memory usage of vector

--- Libs/data_structures/vector.h
+++ Libs/data_structures/vector.h
@@ -1,1 +1,17 @@
-struct vector;
+#include "stdbool.h"
+
+typedef struct vector{
+    int *data;
+    size_t size;
+    size_t capacity;
+} vector;
+
+vector createVector(size_t n);
+
+void reserve(vector *v, size_t newCapacity);
+
+void clear(vector *v);
+
+void shrinkToFit(vector *v);
+
+void deleteVector(vector *v);
--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#include <stdint.h>
 #include "libs/data_structures/vector.h"
+#define SIZE_MAX 128
 
 int main() {
-    printf("Hello, World!\n");
+
+    vector v = createVector(0);
+
     return 0;
 }
