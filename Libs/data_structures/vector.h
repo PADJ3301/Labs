27fe8d88b934f6b73a5721b06ea812cd3b5dd781
PADJ3301 <danielle.chmyhov@gmail.com>
2023-04-16 18:03:32 +0300

    append access functions

--- Libs/data_structures/vector.h
+++ Libs/data_structures/vector.h
@@ -27,3 +27,17 @@ void test_pushBack_emptyVector();
 void test_pushBack_fullVector();
 
 void test_popBack_notEmptyVector();
+
+int* atVector(vector *v, size_t index);
+
+int* back(vector *v);
+
+int* front(vector *v);
+
+void test_atVector_notEmptyVector();
+
+void test_atVector_requestToLastElement();
+
+void test_back_oneElementInVector();
+
+void test_front_oneElementInVector();
